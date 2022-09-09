7f2b4f394c6901bab2e66b01503ee06e3c7eed3a
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-09-10 02:35:17 +0900

    eslintとprettierを導入

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,13 @@
-const getValue = (el: HTMLInputElement) => el.checkValidity() ? Number(el.value) : null
-const getID = () => getValue(document.getElementById('id-input') as HTMLInputElement)?.toString().padStart(6,'0')
-const getIVs = () => ['h','a','b','c','d','s'].map(_ => getValue(document.getElementById(`ivs-${_}`) as HTMLInputElement))
+const getValue = (el: HTMLInputElement) =>
+  el.checkValidity() ? Number(el.value) : null
+const getID = () =>
+  getValue(document.getElementById('id-input') as HTMLInputElement)
+    ?.toString()
+    .padStart(6, '0')
+const getIVs = () =>
+  ['h', 'a', 'b', 'c', 'd', 's'].map((_) =>
+    getValue(document.getElementById(`ivs-${_}`) as HTMLInputElement)
+  )
 const addRow = (txt: string, onClick?: () => void) => {
   const div = document.createElement('div')
   const classes = ['message', 'card']
@@ -10,6 +17,7 @@ const addRow = (txt: string, onClick?: () => void) => {
   }
   div.className = classes.join(' ')
   div.append(txt)
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   document.getElementById('message-container')!.appendChild(div)
   scrollToBottom()
 }
@@ -17,7 +25,7 @@ const scrollToBottom = () => {
   const el = document.documentElement
   window.scroll({
     top: el.scrollHeight - el.clientHeight,
-    behavior: 'smooth'
+    behavior: 'smooth',
   })
 }
 
@@ -29,20 +37,20 @@ type Result = {
 }
 
 let worker: Worker | null = null
-const createWorker = (id: string, version: string) => {
+const createWorker = (id: string) => {
   worker = new Worker('./js/worker.js')
   let found = false
   worker.addEventListener('message', (e) => {
-    if (e.data === 'completed'){
-        document.getElementById('start-button')!.removeAttribute('disabled')
-        addRow('計算を終了しました')
-        if (found) {
-          addRow('結果をクリック/タップするとツイートできます')
-        }
-    }
-    else {
+    if (e.data === 'completed') {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      document.getElementById('start-button')!.removeAttribute('disabled')
+      addRow('計算を終了しました')
+      if (found) {
+        addRow('結果をクリック/タップするとツイートできます')
+      }
+    } else {
       found = true
-      const {seed, index, tsv, trv} = e.data as Result
+      const { seed, index, tsv, trv } = e.data as Result
       addRow(`${seed} ${index}[F] tsv=${tsv} trv=${trv}`, () => {
         const tweet = `ID ${id} のセーブデータのTSVを特定したよ！ seed=${seed} TSV=${tsv} TRV=${trv}`
         const shareURL = `https://twitter.com/intent/tweet?text=${tweet}`
@@ -58,7 +66,7 @@ const fetchSeedList = async (id: string, version: 'sm' | 'usum') => {
   const url = `https://jyk6mdg08i.execute-api.ap-northeast-1.amazonaws.com/rng/gen7/tsvsupport?version=${version}&g7tid=${id}`
   return fetch(url)
     .then((res) => {
-      if(!res.ok) throw new Error(`${res.status} ${res.statusText}`)
+      if (!res.ok) throw new Error(`${res.status} ${res.statusText}`)
       return res.json()
     })
     .catch((e) => {
@@ -67,38 +75,56 @@ const fetchSeedList = async (id: string, version: 'sm' | 'usum') => {
     })
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function run() {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   document.getElementById('start-button')!.setAttribute('disabled', 'true')
 
-  const version = (document.getElementById('ver-sm') as HTMLInputElement).checked
-    ? 'sm' : 'usum'
+  const version = (document.getElementById('ver-sm') as HTMLInputElement)
+    .checked
+    ? 'sm'
+    : 'usum'
 
-    const id = getID();
-    if (id == null) return alert('IDの入力にエラーがあります')
+  const id = getID()
+  if (id == null) return alert('IDの入力にエラーがあります')
 
-    const ivs = getIVs();
-    if (ivs.includes(null)) return alert('個体値の入力にエラーがあります')
+  const ivs = getIVs()
+  if (ivs.includes(null)) return alert('個体値の入力にエラーがあります')
 
-    const nature = Number((document.getElementById('nature-select') as HTMLSelectElement).value)
+  const nature = Number(
+    (document.getElementById('nature-select') as HTMLSelectElement).value
+  )
 
-    const max = getValue(document.getElementById('search-max') as HTMLInputElement)
-    if (max == null) return alert('上限の入力にエラーがあります')
+  const max = getValue(
+    document.getElementById('search-max') as HTMLInputElement
+  )
+  if (max == null) return alert('上限の入力にエラーがあります')
 
-    fetchSeedList(id, version)
-      .then((seedList) => {
-        createWorker(id, version).postMessage({ method: 'start', version, ivs, nature, seedList, max })
-        addRow('計算を開始しました')
+  fetchSeedList(id, version)
+    .then((seedList) => {
+      createWorker(id).postMessage({
+        method: 'start',
+        version,
+        ivs,
+        nature,
+        seedList,
+        max,
       })
-      .catch(() => {
-        alert('seedリストの取得に失敗しました (・ω<)')
-        document.getElementById('start-button')!.removeAttribute('disabled')
-      })
+      addRow('計算を開始しました')
+    })
+    .catch(() => {
+      alert('seedリストの取得に失敗しました (・ω<)')
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      document.getElementById('start-button')!.removeAttribute('disabled')
+    })
 }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function cancel() {
   if (worker) {
     worker.terminate()
-    worker = null;
-    
+    worker = null
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     document.getElementById('start-button')!.removeAttribute('disabled')
     addRow('計算を中止しました')
   }
--- src/ts/worker.ts
+++ src/ts/worker.ts
@@ -1,17 +1,17 @@
-"use strict";
+'use strict'
 if (!Math.imul) {
-  Math.imul = function(opA, opB) {
-      opB |= 0
-      let result = (opA & 0x003fffff) * opB
-      if (opA & 0xffc00000) result += (opA & 0xffc00000) * opB |0;
-      return result |0;
+  Math.imul = function (opA, opB) {
+    opB |= 0
+    let result = (opA & 0x003fffff) * opB
+    if (opA & 0xffc00000) result += ((opA & 0xffc00000) * opB) | 0
+    return result | 0
   }
 }
 
-const MSK1 = 0xdfffffef;
-const MSK2 = 0xddfecb7f;
-const MSK3 = 0xbffaffff;
-const MSK4 = 0xbffffff6;
+const MSK1 = 0xdfffffef
+const MSK2 = 0xddfecb7f
+const MSK3 = 0xbffaffff
+const MSK4 = 0xbffffff6
 
 class SFMT {
   private stateVector: number[]
@@ -22,74 +22,110 @@ class SFMT {
     this.stateVector = [] // 624
 
     this.stateVector[0] = seed >>> 0
-    for (let i = 1; i < 624; i++) 
-      this.stateVector[i] = (Math.imul(0x6C078965, this.stateVector[i - 1] ^ (this.stateVector[i - 1] >>> 30)) + i) & 0xFFFFFFFF
+    for (let i = 1; i < 624; i++)
+      this.stateVector[i] =
+        (Math.imul(
+          0x6c078965,
+          this.stateVector[i - 1] ^ (this.stateVector[i - 1] >>> 30)
+        ) +
+          i) &
+        0xffffffff
 
     this.periodCertification()
 
     this.randIndex = 624
 
-    this.index32 = 0;
+    this.index32 = 0
   }
 
   periodCertification() {
-    const PARITY = [1,0,0,0x13c9e684]
-  
-    let inner = 0;
-    for (let i = 0; i < 4; i++) inner ^= this.stateVector[i] & PARITY[i];
-    for (let i = 16; i > 0; i >>= 1) inner ^= inner >> i;
-  
+    const PARITY = [1, 0, 0, 0x13c9e684]
+
+    let inner = 0
+    for (let i = 0; i < 4; i++) inner ^= this.stateVector[i] & PARITY[i]
+    for (let i = 16; i > 0; i >>= 1) inner ^= inner >> i
+
     // check OK
-    if ((inner & 1) == 1) return;
-  
+    if ((inner & 1) == 1) return
+
     // check NG, and modification
     for (let i = 0; i < 4; i++) {
-      let work = 1;
+      let work = 1
       for (let j = 0; j < 32; j++, work <<= 1) {
         if ((work & PARITY[i]) != 0) {
-          this.stateVector[i] ^= work;
-          return;
+          this.stateVector[i] ^= work
+          return
         }
       }
     }
   }
 
   getRand32() {
-    if (this.randIndex >= 624){
-        this.generateRandAll();
-        this.randIndex = 0;
+    if (this.randIndex >= 624) {
+      this.generateRandAll()
+      this.randIndex = 0
     }
 
-    this.index32++;
-    return this.stateVector[this.randIndex++];
+    this.index32++
+    return this.stateVector[this.randIndex++]
   }
 
   getRand64() {
     return [this.getRand32(), this.getRand32()]
   }
 
-  getIndex = () => this.index32 >>> 1;
+  getIndex = () => this.index32 >>> 1
 
   private generateRandAll() {
-    const p = this.stateVector;
+    const p = this.stateVector
 
-    let a = 0;
-    let b = 488;
-    let c = 616;
-    let d = 620;
-    do
-    {
-      p[a + 3] = p[a + 3] ^ (p[a + 3] << 8) ^ (p[a + 2] >>> 24) ^ (p[c + 3] >>> 8) ^ (((p[b + 3] >>> 11) & MSK4) >>> 0) ^ (p[d + 3] << 18);
-      p[a + 2] = p[a + 2] ^ (p[a + 2] << 8) ^ (p[a + 1] >>> 24) ^ (p[c + 3] << 24) ^ (p[c + 2] >>> 8) ^ (((p[b + 2] >>> 11) & MSK3) >>> 0) ^ (p[d + 2] << 18);
-      p[a + 1] = p[a + 1] ^ (p[a + 1] << 8) ^ (p[a + 0] >>> 24) ^ (p[c + 2] << 24) ^ (p[c + 1] >>> 8) ^ (((p[b + 1] >>> 11) & MSK2) >>> 0) ^ (p[d + 1] << 18);
-      p[a + 0] = p[a + 0] ^ (p[a + 0] << 8) ^ (p[c + 1] << 24) ^ (p[c + 0] >>> 8) ^ (((p[b + 0] >>> 11) & MSK1) >>> 0) ^ (p[d + 0] << 18);
-      c = d; d = a; a += 4; b += 4;
-      if (b >= 624) b = 0;
-    } while (a < 624);
+    let a = 0
+    let b = 488
+    let c = 616
+    let d = 620
+    do {
+      p[a + 3] =
+        p[a + 3] ^
+        (p[a + 3] << 8) ^
+        (p[a + 2] >>> 24) ^
+        (p[c + 3] >>> 8) ^
+        (((p[b + 3] >>> 11) & MSK4) >>> 0) ^
+        (p[d + 3] << 18)
+      p[a + 2] =
+        p[a + 2] ^
+        (p[a + 2] << 8) ^
+        (p[a + 1] >>> 24) ^
+        (p[c + 3] << 24) ^
+        (p[c + 2] >>> 8) ^
+        (((p[b + 2] >>> 11) & MSK3) >>> 0) ^
+        (p[d + 2] << 18)
+      p[a + 1] =
+        p[a + 1] ^
+        (p[a + 1] << 8) ^
+        (p[a + 0] >>> 24) ^
+        (p[c + 2] << 24) ^
+        (p[c + 1] >>> 8) ^
+        (((p[b + 1] >>> 11) & MSK2) >>> 0) ^
+        (p[d + 1] << 18)
+      p[a + 0] =
+        p[a + 0] ^
+        (p[a + 0] << 8) ^
+        (p[c + 1] << 24) ^
+        (p[c + 0] >>> 8) ^
+        (((p[b + 0] >>> 11) & MSK1) >>> 0) ^
+        (p[d + 0] << 18)
+      c = d
+      d = a
+      a += 4
+      b += 4
+      if (b >= 624) b = 0
+    } while (a < 624)
   }
 
   toString() {
-    return this.stateVector.map(_ => (_ >>> 0).toString(16).padStart(8, '0')).join(',')
+    return this.stateVector
+      .map((_) => (_ >>> 0).toString(16).padStart(8, '0'))
+      .join(',')
   }
 }
 class SFMTStream {
@@ -98,32 +134,30 @@ class SFMTStream {
   private nextIV: number
   constructor(seed: number) {
     this.sfmt = new SFMT(seed)
-    this.ivsCode = 0;
+    this.ivsCode = 0
 
-    for (let i=0; i<6; i++) {
+    for (let i = 0; i < 6; i++) {
       const [l] = this.sfmt.getRand64()
-      this.ivsCode = ((this.ivsCode << 5) | ((l>>>0) & 0x1F)) & 0x3FFFFFFF
+      this.ivsCode = ((this.ivsCode << 5) | ((l >>> 0) & 0x1f)) & 0x3fffffff
     }
-    this.nextIV = this.sfmt.getRand64()[0] & 0x1F
+    this.nextIV = this.sfmt.getRand64()[0] & 0x1f
   }
-  
-  get(): [number,number] {
+
+  get(): [number, number] {
     const [l, h] = this.sfmt.getRand64()
-    const nature = ((h>>>0) * 21 + (l>>>0)) % 25
+    const nature = ((h >>> 0) * 21 + (l >>> 0)) % 25
     const ivs = this.ivsCode
-    this.ivsCode = ((this.ivsCode << 5) | this.nextIV) & 0x3FFFFFFF
-    this.nextIV = (l & 0x1F)
+    this.ivsCode = ((this.ivsCode << 5) | this.nextIV) & 0x3fffffff
+    this.nextIV = l & 0x1f
 
     return [ivs, nature]
   }
   getId(): number {
-    const [l, h] = this.sfmt.getRand64()
-    const nature = ((h>>>0) * 21 + (l>>>0)) % 25
-    const ivs = this.ivsCode
-    this.ivsCode = ((this.ivsCode << 5) | this.nextIV) & 0x3FFFFFFF
-    this.nextIV = (l & 0x1F)
+    const [l] = this.sfmt.getRand64()
+    this.ivsCode = ((this.ivsCode << 5) | this.nextIV) & 0x3fffffff
+    this.nextIV = l & 0x1f
 
-    return (l & 0xFFFF) ^ (l >>> 16)
+    return (l & 0xffff) ^ (l >>> 16)
   }
 
   toString = () => this.sfmt.toString()
@@ -131,13 +165,12 @@ class SFMTStream {
 }
 
 const encode = (ivs: number[]) => {
-    let code = 0;
-    for (let i = 0; i < 6; i++)
-        code = (code << 5) | ivs[i];
-    return code;
-};
+  let code = 0
+  for (let i = 0; i < 6; i++) code = (code << 5) | ivs[i]
+  return code
+}
 
-self.addEventListener("message", (e) => {
+self.addEventListener('message', (e: MessageEvent<Props>) => {
   start(e.data)
 })
 
@@ -148,32 +181,33 @@ type Props = {
   nature: number
   max: number
 }
-const start = ({version, seedList, ivs, nature, max}: Props) => {
+const start = ({ version, seedList, ivs, nature, max }: Props) => {
   const targetIVs = encode(ivs)
   const targetNature = nature
 
-  console.log("start", {'#seeds': seedList.length, ivs, nature })
+  console.log('start', { '#seeds': seedList.length, ivs, nature })
 
-  const init = (version === 'sm' ? 1005 : 1125)
+  const init = version === 'sm' ? 1005 : 1125
 
   for (const seed of seedList) {
-    const rng = new SFMTStream(seed);
-    for (let i=0; i<init; i++) {
+    const rng = new SFMTStream(seed)
+    for (let i = 0; i < init; i++) {
       rng.get()
     }
     const xor = rng.getId()
-    const [tsv, trv] = [xor >> 4, (xor & 0xF).toString(16).toUpperCase()]
-    for (let i = init+1; i <= max; i++) {
-        let [ivs, nature] = rng.get();
-        if (ivs === targetIVs && nature === targetNature) {
-          postMessage({
-            seed: (seed>>>0).toString(16).padStart(8,'0').toUpperCase(), 
-            index: rng.getIndex() - 1,
-            tsv, trv
-          })
-        }
+    const [tsv, trv] = [xor >> 4, (xor & 0xf).toString(16).toUpperCase()]
+    for (let i = init + 1; i <= max; i++) {
+      const [ivs, nature] = rng.get()
+      if (ivs === targetIVs && nature === targetNature) {
+        postMessage({
+          seed: (seed >>> 0).toString(16).padStart(8, '0').toUpperCase(),
+          index: rng.getIndex() - 1,
+          tsv,
+          trv,
+        })
+      }
     }
   }
 
-  self.postMessage("completed")
+  self.postMessage('completed')
 }
